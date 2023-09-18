d0bf0e8e33057dbf5e8d77822b8d49900753c14f
muhammad-tahseen-45 <144385363+muhammad-tahseen-45@users.noreply.github.com>
2023-09-18 13:06:29 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,138 @@
+// //  1
+// for(var i = 1 ; i<=10 ; i++){
+
+
+// console.log(i)
+  
+// }
+
+
+// // 2
+
+// for(var i = 1 ; i<=12 ; i++){
+
+
+//     console.log(i)
+      
+//     }
+
+//     // 3
+
+//     for (var i = 0 ;  i <= 4 ;  i++ ){
+
+//         console.log(i)
+
+//     }
+
+
+//     // 4
+//     for (var i = 0 ;  i <= 100 ;  i++ ){
+
+//         console.log(i)
+
+        
+//     }
+
+
+//     // 5
+
+
+//     for ( var  j = 3 ; j>0 ; j--){
+//         console.log(j)
+//     }
+    
+    
+
+//     // 6
+
+
+
+//     var number = [1,2,3,4,5,6,7,];
+
+//     console.log(number);
+
+
+//     // 7
+
+
+//     var falg = (true);
+//    console.log(falg);
+
+    
+   
+//    //    8
+
+//    var pet = ["dogs","sheep","duks","deer"]
+  
+//    for (q=0 ; q < pet.length; q++){
+
+//     console.log(pet[q] +  "animal")
+//    }
+
+
+// //    9
+
+
+//     for ( var n = 0 ; n<=10 ;  n++){
+//         if(n===2){
+//             alert(n-1)
+//             break
+//         }
+//     }
+
+
+
+
+//   10  
+
+
+   
+
+// var userName = ["yaseen", "mirza", "abdullah " , "ali"];
+//  var firstNAme =  prompt("enter the forst name");
+
+// for( var q = 0 ; q<=userName.length ; q++){
+//     if( firstNAme ===  "username[q]" ){
+//         alert("enter");
+//     }
+//     else{alert("plz write correct username");
+//  }
+// }
+
+
+
+// 11
+
+ var list = [1,2,3,4,5,6,7,8,9,];
+ var matchFound = false;
+
+ var userInput = +prompt("enter the number");
+
+ for ( var u = 0 ; u<list.length ; u++){
+    if(userInput === list[u]){
+        alert("match found");
+        matchFound = true ;
+        break
+ 
+    }
+    else{
+        alert("note found")
+    }
+ }
+
+    
+// 12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
